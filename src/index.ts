905a81bf382a05aee78bae5429e355573a554035
Febiu-dot <febiucontacto@gmail.com>
2026-04-26 12:44:52 -0300

    redeploy

--- src/index.ts
+++ src/index.ts
@@ -74,3 +74,4 @@ httpServer.listen(PORT, () => {
   console.log(`🎱 Servidor corriendo en http://localhost:${PORT}`);
   console.log(`   Entorno: ${process.env.NODE_ENV ?? 'development'}`);
 });
+// redeploy
